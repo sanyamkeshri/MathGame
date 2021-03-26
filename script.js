90c1a969d13d6c14b6dfe6dee54ca182e7314ec6
sanyamkeshri <78680833+sanyamkeshri@users.noreply.github.com>
2021-03-26 13:15:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("P1Input").addEventListener("keydown", (e) => {
 });
 document.getElementById("P2Input").addEventListener("keydown", (e) => {
     if(e.key == "Enter"){
-        input2Text = document.getElementById("P2Input").innerHTML;
+        input1Text = document.getElementById("P1Input").innerHTML;
     }
 });
 
@@ -16,6 +16,12 @@ function Login() {
     if(input1Text != null && input2Text != null) {
         localStorage.setItem("player1", input1Text);
         localStorage.setItem("player2", input2Text);
-        
+    }else {
+        input1Text = document.getElementById("P1Input").innerHTML;
+        input2Text = document.getElementById("P1Input").innerHTML;
+        if(input1Text != null && input2Text != null) {
+            localStorage.setItem("player1", input1Text);
+            localStorage.setItem("player2", input2Text);
+        }
     }
 }
